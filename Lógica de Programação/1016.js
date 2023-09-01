131d46db67e0f14a0dd2140b41b906709c2255bb
Tadeu Anderson <tadeuanderson15@hotmail.com>
2023-09-01 11:49:16 -0300

    novos

---
+++ Lógica de Programação/1016.js
@@ -0,0 +1,8 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+const tempo = (2 * input);
+
+console.log(tempo + " minutos");
